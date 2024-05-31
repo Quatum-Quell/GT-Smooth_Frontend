a2732b86c0681ba528e81f194139e66cc583c211
dooney <delzeen55@gmail.com>
2024-05-31 11:12:12 +0100

    dashboard page setup

---
+++ src/component/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div className="bg-[#181820] sm:w-[375px] h-screen flex flex-col justify-center items-center gap-[78px] pt-[67px] pb-[35px] text-white text-5xl font-bold">
+      Dashboard
+    </div>
+  );
+};
+
+export default Dashboard;
